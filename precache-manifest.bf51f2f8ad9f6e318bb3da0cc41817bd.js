b052aded3c2f5338946d1d2da552ce692de06215
danielnjuguna <danielnjuguna9042@gmail.com>
2020-08-24 01:08:45 -0700

    Updates

---
+++ precache-manifest.bf51f2f8ad9f6e318bb3da0cc41817bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b1fde8066a79d8d0c52e5a23e844a2",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "21706424f66bf3f5924f",
+    "url": "/crown-clothing/static/css/main.3712497d.chunk.css"
+  },
+  {
+    "revision": "185b1859c683a5074478",
+    "url": "/crown-clothing/static/js/2.6d9b6e32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crown-clothing/static/js/2.6d9b6e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21706424f66bf3f5924f",
+    "url": "/crown-clothing/static/js/main.7d7ffd31.chunk.js"
+  },
+  {
+    "revision": "cfabdb4ba54b065a8a4e",
+    "url": "/crown-clothing/static/js/runtime-main.1faa2b66.js"
+  }
+]);
